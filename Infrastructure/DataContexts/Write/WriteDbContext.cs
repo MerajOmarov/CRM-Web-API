f42493ba4eea786e5fde3eaaf252e7a2b3ecd496
Meraj Omarov <meracomarov@gmail.com>
2024-05-03 10:51:04 +0400

    dwdvr

--- Infrastructure/DataContexts/Write/WriteDbContext.cs
+++ Infrastructure/DataContexts/Write/WriteDbContext.cs
@@ -1,13 +1,7 @@
 ﻿using Domen.JWTModels;
 using Domen.Models.CommandModels;
-using Domen.Models.QueryModel;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.DataContexts.CommandDbContext
 {
