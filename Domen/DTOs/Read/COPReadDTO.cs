c318742d9b563771e117e0f33ba17230921f888e
Meraj Omarov <meracomarov@gmail.com>
2024-05-03 10:52:47 +0400

    zx xccx

---
+++ Domen/DTOs/Read/COPReadDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Buisness.DTOs.Query
+{
+    public class COPReadDTO
+    {
+        public Guid CustomerPIN { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSername { get; set; }
+        public Guid OrderCode { get; set; }
+        public string OrderDeedline { get; set; }
+        public Guid ProductBarcode { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public double ProductPrice { get; set; }
+    }
+}
